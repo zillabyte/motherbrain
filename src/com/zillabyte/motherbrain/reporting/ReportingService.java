4328b9405b0a4ae4e4dc31cb3413dd4053b556e7
Benjamin Chan <benjzc@gmail.com>
2014-12-02 08:37:22 -0600

    rm interface file

--- src/com/zillabyte/motherbrain/reporting/ReportingService.java
+++
@@ -1,8 +0,0 @@
-package com.zillabyte.motherbrain.reporting;
-
-import com.zillabyte.motherbrain.api.APIException;
-
-public interface ReportingService {
-
-	void report(String flowId, String message, Exception e) throws APIException;
-}
